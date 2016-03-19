bd4d69885c55e1cf08afdeee49f30e2c741c56c7
Charandeep S. Matta <charandeep.matta@gmail.com>
2016-03-19 20:38:29 +0200

    Disable spring banner

--- src/main/java/com/charandeepmatta/server/SweetBootApplication.java
+++ src/main/java/com/charandeepmatta/server/SweetBootApplication.java
@@ -1,5 +1,7 @@
 package com.charandeepmatta.server;
 
+import static org.springframework.boot.Banner.Mode.OFF;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SweetBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SweetBootApplication.class, args);
+		SpringApplication app = new SpringApplication(SweetBootApplication.class);
+		app.setBannerMode(OFF);
+		app.run(args);
 	}
 
 }
